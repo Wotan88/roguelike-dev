6ff599dbf260d73ce511a4a042d79a45517a5a0f
Awesome2K <qShadowp@gmail.com>
2016-10-28 21:58:15 +0300

    Update stats.hpp

--- include/stats.hpp
+++ include/stats.hpp
@@ -1,4 +1,45 @@
-class Stats{
+class Stats {
+public:
+    // Setters and getters
+    void setStrength(int strength);
+    int getStrength();
+    void setConstitution;
+    int getConstitution();
+    void setDexterity(int dexterity);
+    int getDexterity();
+    void setIntelligence(int intelligence);
+    int getIntelligence();
+    void setWisdom(int wisdom);
+    int getWisdom();
+    void setCharisma(int charisma);
+    int getCharisma();
+    void setHealth(int health);
+    int getHealth();
+    void setMana(int mana);
+    int getMana();
+    void setExp(int exp);
+    int getExp();
+    void setHunger(int hunger);
+    int getHunger();
+    void setThirst(int thirst);
+    int getThirst();
+    void setLvl(int lvl);
+    int getLvl();
+    
+    // Changing stats
+    void addStrength(int strength);
+    void addConstitution(int constitution);
+    void addDexterity(int dexterity);
+    void addIntelligence(int intelligence);
+    void addWisdom(int wisdom);
+    void addCharisma(int charisma);
+    void addHealth(int health);
+    void addMana(int mana);
+    void addHunger(int hunger);
+    void addThirst(int thirst);
+    void addExp(int exp);
+    void addLvl(int lvl);
+
 private:
     int mStrength = 10;
     int mConstitution = 10;
@@ -10,134 +51,10 @@ private:
     int mHealth = 25;
     int mMana = 25;
     
-    int mHunger = 50; //Допустим, что проценты
+    // Assuming this is percentage
+    int mHunger = 50;
     int mThirst = 50;
     
     int mExp = 0;
     int mLvl = 1;
-public:
-        //Setters and getters
-    void setStrength(int strength){
-        mStrength = strength;
-    }
-    int getStrength(){
-        return mStrength;
-    }
-    
-    void setConstitution(int constitution){
-        mConstitution = constitution;
-    }
-    int getConstitution(){
-        return mConstitution;
-    }
-    
-    void setDexterity(int dexterity){
-        mDexterity = dexterity;
-    }
-    int getDexterity(){
-        return mDexterity;
-    }
-    
-    void setIntelligence(int intelligence){
-        mIntelligence = intelligence;
-    }
-    int getIntelligence(){
-        return mIntelligence;
-    }
-    
-    void setWisdom(int wisdom){
-        mWisdom = wisdom;
-    }
-    int getWisdom(){
-        return mWisdom;
-    }
-    
-    void setCharisma(int charisma){
-        mCharisma = charisma;
-    }
-    int getCharisma(){
-        return mCharisma;
-    }
-    
-    void setHealth(int health){
-        mHeatlh = health;
-    }
-    int getHealth(){
-        return mHealth;
-    }
-    
-    void setMana(int mana){
-        mMana = mana;
-    }
-    int getMana(){
-        return mMana;
-    }
-    
-    void setExp(int exp){
-        mExp = exp;
-    }
-    int getExp(){
-        return mExp;
-    }
-    
-    void setHunger(int hunger){
-        mHunger = hunger;
-    }
-    int getHunger(){
-        return mHunger;
-    }
-    
-    void setThirst(int thirst){
-        mThirst = thirst;
-    }
-    int getThirst(){
-        return mThirst;
-    }
-    
-    void setLvl(int lvl){
-        mLvl = lvl;
-    }
-    int getLvl(){
-        return mLvl;
-    }
-        //Setters and getters
-    
-        //Changing stats
-    void AddStrength(int strength){
-        mStrength += strength;
-    }
-    void AddConstitution(int constitution){
-        mConstitution += constitution;
-    }
-    void AddDexterity(int dexterity){
-        mDexterity += dexterity;
-    }
-    void AddIntelligence(int intelligence){
-        mIntelligence += intelligence;
-    }
-    void AddWisdom(int wisdom){
-        mWisdom += wisdom;
-    }
-    void AddCharisma(int charisma){
-        mCharisma += charisma;
-    }
-    void AddHealth(int health){
-        mHealth += health;
-    }
-    void AddMana(int mana){
-        mMana += mana;
-    }
-    void AddHunger(int hunger){
-        mHunger += hunger;
-    }
-    void AddThirst(int thirst){
-        mThirst += thirst;
-    }
-    void AddExp(int exp){
-        mExp += exp;
-    }
-    void AddLvl(int lvl){
-        mLvl += lvl;
-    }
-        //Changing stats
 };
